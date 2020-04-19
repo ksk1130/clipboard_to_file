1a92c1903aa2607b165de609fc326e77774a86e6
KeisukeSasaki <ksk.andr@gmail.com>
2020-04-20 00:09:01 +0900

    クリップボードの内容を新しい⇒古い順でファイル書き込みするよう変更

--- clipboard_to_file.bat
+++ clipboard_to_file.bat
@@ -6,6 +6,12 @@ SET OUTPUT_FILE_NAME=clip_data.txt
 REM コマンドの実行結果を変数に格納
 for /f "usebackq delims=" %%a in (`powershell get-clipboard`) do set CLIP_DATA=%%a
 echo %CLIP_DATA%
-echo %CLIP_DATA% >> %OUTPUT_FILE_DIR%\%OUTPUT_FILE_NAME%
+
+REM 既存のファイル内容をクリップボードに上書き(退避)
+type %OUTPUT_FILE_DIR%\%OUTPUT_FILE_NAME% | clip
+
+REM 新しい⇒古いの順番でファイルに書き込み
+echo %CLIP_DATA% >          %OUTPUT_FILE_DIR%\%OUTPUT_FILE_NAME%
+powershell get-clipboard >> %OUTPUT_FILE_DIR%\%OUTPUT_FILE_NAME%
 
 PAUSE
